a44b78dad2d62b94211440d767b3e8726e5ed5fc
Michael Bade <mbade314@gmail.com>
2020-05-12 20:22:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(int)
+  #if int value is less than 1, 0, or 1 then it's not a prime number.
+  if int <= 1 || int == 0 || int == 1
+    return false
+  #if the number from 2 (the first prime number) up to the integer in question has a remainder of 0, it's NOT a prime number.
+  elsif
+    (2...int - 1).each do |i|
+      if int % i == 0
+        return false
+      end
+    end
+  end
+  #if the number from 2 (the first prime number) up to the integer in question does NOT have a remainder of 0, it IS a prime number
+  true
+end
