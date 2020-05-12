851a6f7cb1f394491bee80780e18b58a3a725bce
Michael Bade <mbade314@gmail.com>
2020-05-12 20:31:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -5,12 +5,15 @@ def prime?(int)
     return false
   #if the number from 2 (the first prime number) up to the integer in question has a remainder of 0, it's NOT a prime number.
   elsif
+    #start at 2, the first prime number and go up to the integer, but not the integer itself. Iterate each number up to the integer (hence, the - 1)
     (2...int - 1).each do |i|
+      #if any iteration has NO remainder...
       if int % i == 0
+        #return false
         return false
       end
     end
   end
-  #if the number from 2 (the first prime number) up to the integer in question does NOT have a remainder of 0, it IS a prime number
+  #if the number from 2 (the first prime number) up to the integer in question HAS a remainder of anything BUT 0, it IS a prime number, so true!
   true
 end
